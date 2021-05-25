1ab15597e82023b0a818ef9dd5403202600dfba5
Maftunbek <maftunbek.r@gmail.com>
2021-05-25 16:23:56 +0500

    Ajax request sample

--- client/src/main/scala/org/informalgo/portfolio/ScalaJSExample.scala
+++ client/src/main/scala/org/informalgo/portfolio/ScalaJSExample.scala
@@ -3,8 +3,29 @@ package org.informalgo.portfolio
 import org.informalgo.portfolio.shared.SharedMessages
 import org.querki.jquery._
 import org.scalajs.dom._
+import scala.scalajs.js
 
 object ScalaJSExample {
+  case class AjaxSuccess(data: js.Any, textStatus: String, jqXHR: JQueryXHR)
+  def resCreator: (js.Any, String, JQueryXHR) => AjaxSuccess = (a, b, c) => AjaxSuccess(a, b, c)
+  def ajaxSettings(url: String, data: String): JQueryAjaxSettings = {
+    def handleAjaxError(jqXHR: JQueryXHR, textStatus: String, errorThrow: String): Unit = {
+      println("Error while performing AJAX POST")
+    }
+
+    def handleAjaxSuccess(data: js.Any, textStatus: String, jqXHR: JQueryXHR): Unit = {
+      val jsonFromServer = jqXHR.responseText
+      println(s"Got from server: $jsonFromServer")
+    }
+
+    js.Dynamic.literal(
+      url = url,
+      contentType = "application/json",
+      data = data,
+      `type` = "POST",
+      success = handleAjaxSuccess _,
+      error = handleAjaxError _).asInstanceOf[JQueryAjaxSettings]
+  }
 
   def main(args: Array[String]): Unit = {
     val preloader = $("#preloader")
@@ -32,5 +53,13 @@ object ScalaJSExample {
 
     companyName.textContent = SharedMessages.companyName
     companyName.classList.add("text-white")
+
+    $("#postButton").click { (_: Event) =>
+      val name = $("#name").valueString
+      $.ajax(ajaxSettings("/contact-us", s"""{"name": "$name"}"""))
+        .done { (_: js.Any, _: String, res: JQueryXHR) =>
+          println(res.responseText)
+        }
+    }
   }
 }
--- server/app/org/informalgo/portfolio/controllers/Application.scala
+++ server/app/org/informalgo/portfolio/controllers/Application.scala
@@ -3,21 +3,30 @@ package org.informalgo.portfolio.controllers
 import com.typesafe.scalalogging.LazyLogging
 import org.informalgo.portfolio.shared.SharedMessages
 import org.webjars.play.WebJarsUtil
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc._
 import views.html.index
 
 import javax.inject._
 import scala.concurrent.ExecutionContext
-
+import play.filters.csrf._
+import play.filters.csrf.CSRF.Token
 @Singleton
 class Application @Inject()(val controllerComponents: ControllerComponents,
                             indexTemplate: index,
-                            implicit val webJarsUtil: WebJarsUtil)
+                            implicit val webJarsUtil: WebJarsUtil,
+                            addToken: CSRFAddToken, checkToken: CSRFCheck)
                            (implicit val ec: ExecutionContext)
   extends BaseController with LazyLogging {
 
   def index: Action[AnyContent] = Action {
     Ok(indexTemplate())
+  }
+
+  def contactUs: Action[JsValue] = Action(parse.json) { implicit request =>
+    val name = (request.body \ "name").as[String]
+    println(name)
+    Ok(Json.toJson("OK"))
   }
 
 }
